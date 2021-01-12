c3a39181e85702b84ccbde2d6d98e3d0ed9013b8
Thomas Stolz <thomas.stolz@tum.de>
2021-01-12 16:08:35 +0100

    customized the web interface

--- static/app.js
+++ static/app.js
@@ -1,73 +1,3 @@
-// function to calculate background gradient from the wavelength value
-function nmToRGB(wavelength){
-    var Gamma = 0.80,
-    IntensityMax = 255,
-    factor, red, green, blue;
-    if((wavelength >= 380) && (wavelength<440)){
-        red = -(wavelength - 440) / (440 - 380);
-        green = 0.0;
-        blue = 1.0;
-    }else if((wavelength >= 440) && (wavelength<490)){
-        red = 0.0;
-        green = (wavelength - 440) / (490 - 440);
-        blue = 1.0;
-    }else if((wavelength >= 490) && (wavelength<510)){
-        red = 0.0;
-        green = 1.0;
-        blue = -(wavelength - 510) / (510 - 490);
-    }else if((wavelength >= 510) && (wavelength<580)){
-        red = (wavelength - 510) / (580 - 510);
-        green = 1.0;
-        blue = 0.0;
-    }else if((wavelength >= 580) && (wavelength<645)){
-        red = 1.0;
-        green = -(wavelength - 645) / (645 - 580);
-        blue = 0.0;
-    }else if((wavelength >= 645) && (wavelength<781)){
-        red = 1.0;
-        green = 0.0;
-        blue = 0.0;
-    }else{
-        red = 0.0;
-        green = 0.0;
-        blue = 0.0;
-    };
-    // Let the intensity fall off near the vision limits
-    if((wavelength >= 380) && (wavelength<420)){
-        factor = 0.3 + 0.7*(wavelength - 380) / (420 - 380);
-    }else if((wavelength >= 420) && (wavelength<701)){
-        factor = 1.0;
-    }else if((wavelength >= 701) && (wavelength<781)){
-        factor = 0.3 + 0.7*(780 - wavelength) / (780 - 700);
-    }else{
-        factor = 0.0;
-    };
-    if (red !== 0){
-        red = Math.round(IntensityMax * Math.pow(red * factor, Gamma));
-    }
-    if (green !== 0){
-        green = Math.round(IntensityMax * Math.pow(green * factor, Gamma));
-    }
-    if (blue !== 0){
-        blue = Math.round(IntensityMax * Math.pow(blue * factor, Gamma));
-    }
-    return [red,green,blue];
-}
-
-// applying background gradient
-function makebg(element, wavelength){
-    var v = nmToRGB(wavelength);
-    var color = d3.rgb(v[0], v[1], v[2]);
-    var c1 = color;
-    var c2 = color.darker(2);
-    element.css({
-        background: "linear-gradient(135deg, "+c2+" 0%,"+c1+" 100%)"
-    });
-    if(!element.hasClass("colored")){
-        element.addClass("colored");
-    }
-}
-
 // resetting background gradient
 function resetbg(element){
     if(element.hasClass("colored")){
@@ -89,20 +19,28 @@ function parseData(d){
         var element = $('#wl'+ch).parent();
         if(d[ch]>100){
             var wl = d[ch].toFixed(precision);
-            $('#wl'+ch).html(wl);
-            // recalc background only if wavelength changed by 1nm or more
-            if(Math.abs(d[ch]-oldData[ch])>1){
-                oldData[ch] = d[ch];
-                var c = $.grep(channels, function(e){ return (e.i == ch) && e.background; });
-                if(c.length > 0){
-                    if(!element.hasClass("colored")){
-                        element.css({"background": c[0].background});
-                        element.addClass("colored");
-                    }
-                }else{
-                    makebg(element, d[ch]);
-                }
-            }
+			var freq = (1e-12*299792458/(wl*1e-9)).toFixed(precision);
+			// the channel from the json config file
+			var c = $.grep(channels, function(e){ return (e.i == ch)}); 
+			if(c.length == 0){
+				continue; // channel not present in config file
+			}
+			if("background" in c[0]){
+				if(!element.hasClass("colored")){
+					element.addClass("colored");
+				}
+				element.css({"background": c[0].background});
+			}
+			if("target_THz" in c[0]){
+				var detuning = (1e6*(freq - c[0].target_THz)).toFixed(0);
+				$('#wl'+ch).html((detuning<=0?"":"+") + detuning + " MHz");
+				if(Math.abs(detuning)< 10){
+					element.css({"background": "#0A0"});
+				}
+			}else{
+				$('#wl'+ch).html(freq + " THz");
+			}
+
         }else{
             $('#wl'+ch).html("No data");
             oldData[ch] = d[ch];
